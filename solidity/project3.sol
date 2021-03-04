fd45902f6ad7c734544c61a1889d7138b8363662
David G <davidrgayman@gmail.com>
2021-03-03 20:13:00 -0500

    Changed to accept portfolio strings.

--- solidity/project3.sol
+++ solidity/project3.sol
@@ -1,22 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.4;
 pragma experimental ABIEncoderV2;
-/*
-{
-    “id”:1,
-    “date”:“2021-01-02”,
-    “assets”: [
-        {
-            “asset_name”:“TSLA”,
-            “allocation”:100
-        },
-        {
-            “asset_name”:“AAPL”,
-            “allocation”:50
-        }
-    ]
-}
-*/
+
 contract project3 {
 
     struct Portfolio {
@@ -26,26 +11,77 @@ contract project3 {
         uint id;
     }
 
+    mapping(uint => string) public portfolio_strings;
     mapping(uint => Portfolio) public portfolios;
 
     constructor() {}
 
-    function registerPortfolio_testing(uint id) public returns(string memory) {
-        return "This is a test.";
+    // -------------------------------------------------------------------------
+    // String-based portfolio map functions
+    // -------------------------------------------------------------------------
+
+    function registerPortfolioString(uint id, string memory portfolio_str) public returns(bool) {
+      /** @dev Registers a portfolio allocation in string form.
+        * @param id The portfolio id.
+        * @param portfolio The recommended portfolio allocations in string form.
+        * @return result True if success; false if string is empty.
+        */
+        // TODO Error handling - return fals if empty string.
+        portfolio_strings[id] = portfolio_str;
+        return true;
     }
 
-    function registerPortfolio(uint id, Portfolio memory portfolio) public returns(bool) {
-      /** @dev Registers a suggested portfolio allocation.
+    function getPortfolioString(uint id) public returns(string memory) {
+      /** @dev Retrieves a previously-registered portfolio allocation.
         * @param id The portfolio id.
-        * @param portfolio The recommended portfolio allocations.
+        * @return portfolio The recommended portfolio allocations in string form; empty string if no portfolio registered with the id.
+        */
+        // TODO Error handling.
+        return portfolio_strings[id];
+    }
+
+    // -------------------------------------------------------------------------
+    // Struct-based portfolio map functions
+    // -------------------------------------------------------------------------
+
+    function registerPortfolio(uint id, Portfolio memory portfolio) public returns(bool) {
+      /** @dev Registers a portfolio allocation.
+        * @param id The portfolio id.
+        * @param portfolio The recommended portfolio allocation.
         * @return result Success indicator.
         */
         portfolios[id] = portfolio;
         return true;
     }
 
+    function getPortfolio(uint id) public returns(Portfolio memory) {
+      /** @dev Retrieves a previously-registered portfolio allocation.
+        * @param id The portfolio id.
+        * @return portfolio The recommended portfolio allocation.
+        */
+        return portfolios[id];
+    }
+
     // NOTES
     // Receive JSON object to contract
     // Store struct on chain
     // Error checking to revert changes, if invalid values are found in JSON
+
+    /*
+    {
+        “id”:1,
+        “date”:“2021-01-02”,
+        “assets”: [
+            {
+                “asset_name”:“TSLA”,
+                “allocation”:100
+            },
+            {
+                “asset_name”:“AAPL”,
+                “allocation”:50
+            }
+        ]
+    }
+    */
+
 }
