66012b379184c77ce3ad7637941fdca598324fc3
David G <davidrgayman@gmail.com>
2021-03-08 14:44:15 -0500

    Updated combined contract, tested except return values not working.

--- solidity/Project3PortfolioBuilderSmartContract.sol
+++ solidity/Project3PortfolioBuilderSmartContract.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.5.17;
+pragma solidity ^0.5.4;
 pragma experimental ABIEncoderV2;
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.0.0-beta.0/contracts/token/ERC721/ERC721Full.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.0.0-beta.0/contracts/drafts/Counters.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/drafts/Counters.sol";
 
 contract Project3PortfolioBuilderSmartContract is ERC721Full {
 
@@ -25,28 +25,41 @@ contract Project3PortfolioBuilderSmartContract is ERC721Full {
     // Struct-based portfolio map functions
     // -------------------------------------------------------------------------
 
-    function registerPortfolio(string memory portfolio_uri, address owner, string memory date, string memory name, string memory portfolio_string) public returns(uint) {
+    function registerPortfolio(bool generate_token_id, uint token_id,
+            string memory portfolio_uri, address owner,
+            string memory date, string memory name, string memory portfolio_string) public returns(uint) {
+
       /** @dev Registers a portfolio allocation.
         * @param id The portfolio id.
         * @param portfolio The recommended portfolio allocation.
         * @return result Success indicator.
         */
 
-        token_ids.increment();
-        uint token_id = token_ids.current();
+        if (generate_token_id) {
+            token_ids.increment();
+            token_id = token_ids.current();
+        }
 
-        _mint(owner,token_id);  // TODO
-        _setTokenURI(token_id, portfolio_uri);  // TODO
+        // _mint(owner,token_id);  // TODO
+        // _setTokenURI(token_id, portfolio_uri);  // TODO
 
         portfolios[token_id] = Portfolio(date, name, portfolio_string);
 
-        emit PortfolioEvent(token_id, portfolio_uri);
+        // emit PortfolioEvent(token_id, portfolio_uri);
 
         return token_id;
     }
 
+    function getLastTokenID() public returns (uint) {
+        return token_ids.current();
+    }
+
+    function testGetPortfoioString() public returns (string memory) {
+        return "AAPL: 45";
+    }
+
     // experimental, for testing
-    function registerPortfolio(uint token_id, string memory portfolio_uri, string memory tester) public returns(string memory) {
+    function registerPortfolioTesting(uint token_id, string memory portfolio_uri, string memory tester) public returns(string memory) {
         portfolios[token_id].portfolio_string = tester;
         emit PortfolioEvent(token_id,portfolio_uri);
         return portfolios[token_id].portfolio_string; // for testing
