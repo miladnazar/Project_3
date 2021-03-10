c61b023dd9d2bb057e2c4271518daf25ed617887
David G <davidrgayman@gmail.com>
2021-03-09 21:01:52 -0500

    Preparing to deploy.

--- python/src/build-and-deploy-aws-package.sh
+++ python/src/build-and-deploy-aws-package.sh
@@ -3,8 +3,8 @@
 # Description: https://docs.aws.amazon.com/lambda/latest/dg/python-package.html
 
 # Package the code
-mv litquidity-chatbot.zip .old.litquidity-chatbot.zip
-zip litquidity-chatbot.zip -r lambda_function.py main/
+mv project3-chatbot.zip .old.project3-chatbot.zip
+zip project3-chatbot.zip -r lambda_function.py main/
 
 # Download dependencies to local directory - Targeted directory
 pip install --target ./.package requests
@@ -14,8 +14,9 @@ pip install --target ./.package pandas
 pip install --target ./.package statsmodels
 
 # Add the dependencies to the package root
+mkdir .package
 cd .package
-zip -r ../litquidity-chatbot.zip .
+zip -r ../project3-chatbot.zip .
 cd ..
 
 # Upload and deploy the function code to AWS Lambda
